746eb6dd91ca4088b2a837828e393eebb101399b
baraah <baraahmufrij@gmail.com>
2025-05-20 22:30:43 +0300

    Create Untitled-1.java

---
+++ _book_production/nested_proportions_files/figure-html/Untitled-1.java
@@ -0,0 +1,49 @@
+
+System.out.println(
+    "-------------CREATE GROCERY ITEMS-------------------------------\n" +
+    "Data creation started...\n" +
+    "Data creation complete...\n\n" +
+    
+    "----------------SHOW ALL GROCERY ITEMS---------------------------\n\n" +
+    "Item Name: Whole Wheat Biscuit, \n" +
+    "Item Quantity: 5, \n" +
+    "Item Category: snacks\n\n" +
+
+    "Item Name: XYZ Kodo Millet healthy, \n" +
+    "Item Quantity: 2, \n" +
+    "Item Category: millets\n\n" +
+
+    "Item Name: Dried Whole Red Chilli, \n" +
+    "Item Quantity: 2, \n" +
+    "Item Category: spices\n\n" +
+
+    "Item Name: Healthy Pearl Millet, \n" +
+    "Item Quantity: 1, \n" +
+    "Item Category: millets\n\n" +
+
+    "Item Name: Bonny Cheese Crackers Plain, \n" +
+    "Item Quantity: 6, \n" +
+    "Item Category: snacks\n\n" +
+
+    "--------------GET ITEM BY NAME-----------------------------------\n\n" +
+    "Getting item by name: Whole Wheat Biscuit\n" +
+    "Item Name: Whole Wheat Biscuit, \n" +
+    "Item Quantity: 5, \n" +
+    "Item Category: snacks\n\n" +
+
+    "-----------GET ITEMS BY CATEGORY---------------------------------\n\n" +
+    "Getting items for the category millets\n" +
+    "Name: XYZ Kodo Millet healthy, Quantity: 2\n" +
+    "Name: Healthy Pearl Millet, Quantity: 1\n\n" +
+
+    "-----------UPDATE CATEGORY NAME OF SNACKS CATEGORY----------------\n\n" +
+    "Successfully updated 2 items.\n\n" +
+
+    "----------DELETE A GROCERY ITEM----------------------------------\n\n" +
+    "Item with id Kodo Millet deleted...\n\n" +
+
+    "------------FINAL COUNT OF GROCERY ITEMS-------------------------\n\n" +
+    "Number of documents in the collection = 4\n\n" +
+
+    "-------------------THANK YOU---------------------------"
+);
